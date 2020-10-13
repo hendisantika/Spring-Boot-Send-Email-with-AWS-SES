b58758f5508a832ba9d389733df463d303be90f1
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-14 06:13:30 +0700

    #10 - Add sendEmail method in EmailSenderService

--- src/main/java/com/hendisantika/sendmailwithses/service/EmailSenderService.java
+++ src/main/java/com/hendisantika/sendmailwithses/service/EmailSenderService.java
@@ -1,8 +1,16 @@
 package com.hendisantika.sendmailwithses.service;
 
+import com.hendisantika.sendmailwithses.entity.Mail;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +31,30 @@ public class EmailSenderService {
     public EmailSenderService(JavaMailSender javaMailSender, SpringTemplateEngine templateEngine) {
         this.javaMailSender = javaMailSender;
         this.templateEngine = templateEngine;
+    }
+
+    @Async
+    public void sendEmail(Mail mail) throws MessagingException {
+        MimeMessage message = getMimeMessage(mail);
+        javaMailSender.send(message);
+    }
+
+    private MimeMessage getMimeMessage(Mail mail) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper =
+                new MimeMessageHelper(
+                        message,
+                        MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                        StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariables(mail.getModel());
+        String html = templateEngine.process("email-template", context);
+
+        helper.setTo(mail.getTo());
+        helper.setText(html, true);
+        helper.setSubject(mail.getSubject());
+        helper.setFrom(mail.getFrom());
+        return message;
     }
 }
